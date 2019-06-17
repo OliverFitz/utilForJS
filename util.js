6ba6ca6fd40e732599a5e534b8dcd3f84e89f834
OlivierHu <1124249668@qq.com>
2019-06-17 17:58:01 +0800

    add uuid

--- util.js
+++ util.js
@@ -170,6 +170,24 @@ Class Util {
     if((value - 0) === 0) return '0'
     value = value - 0
     return value.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,')
+  },
+
+  /**
+   * uuid
+   * @return { String }
+   */
+  uuid() {
+    let i, random
+	  let uuid = ''
+
+    for (i = 0; i < 32; i++) {
+      random = Math.random() * 16 | 0;
+      if (i === 8 || i === 12 || i === 16 || i === 20) {
+        uuid += '-'
+      }
+      uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16)
+    }
+    return uuid
   }
 };
 
